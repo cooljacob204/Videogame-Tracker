462f83f07a1f5deedbaef4efcf544acf0111dcd5
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-31 00:24:54 -0400

    Configures app.rb

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
 require "sinatra"
 require "sinatra/activerecord"
+require 'dotenv/load'
 
-set :database_file, "./db/config.yml"
-
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "app/controllers", "*.rb")].each {|f| require f}
 
 class MyApp < Sinatra::Application
-  enable :sessions
+  configure do
+    set :database_file, "db/config.yml"
+    set :public_folder, 'app/public'
+    set :views, "app/views"
+		set :session_secret, "password_security"
+		enable :sessions
+	end
 
+  get '/' do
+    erb :index
+  end
 end
