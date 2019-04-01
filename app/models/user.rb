bb4272ad4ea5f6b27502608168498757242d6b91
Jacob Feldman <jacobgfeld@gmail.com>
2019-04-01 15:33:41 -0400

    Fixes validation to only check on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  validates_presence_of :firstname, :lastname, :email, :password
+  validates_presence_of :firstname, :lastname, :email, :password, on: :create
   has_secure_password
 end
