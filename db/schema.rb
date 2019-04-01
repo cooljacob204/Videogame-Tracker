d8151303b8a05bcc38c99dd7e6ce3d64cb4a96fc
Jacob Feldman <jacobgfeld@gmail.com>
2019-04-01 15:34:53 -0400

    adds a session_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_025237) do
+ActiveRecord::Schema.define(version: 2019_04_01_182810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_025237) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "session_id"
   end
 
 end
