bb7c234dfe41fc43c6d9c38c175fe47ad748b934
Jacob Feldman <jacobgfeld@gmail.com>
2019-04-01 19:35:53 -0400

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_182810) do
+ActiveRecord::Schema.define(version: 2019_04_01_222357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_04_01_182810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "session_id"
+    t.integer "approval"
   end
 
 end
