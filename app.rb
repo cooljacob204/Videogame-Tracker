abfb6dfbe02100b9ff218eec51cd63e897bf675d
Jacob Feldman <jacobgfeld@gmail.com>
2019-04-01 17:55:09 -0400

    Changes quotes to match

--- app.rb
+++ app.rb
@@ -1,18 +1,18 @@
 require 'sinatra'
-require "sinatra/activerecord"
+require 'sinatra/activerecord'
 require 'dotenv/load'
 require 'require_all'
 
 require_all 'app/models'
 require_all 'app/middlewares'
-set :database_file, "db/config.yml"
+set :database_file, 'db/config.yml'
 
 
 class MyApp < Sinatra::Base
   configure do
     set :public_folder, 'app/public'
-    set :views, "app/views"
-    set :session_secret, "password_security"
+    set :views, 'app/views'
+    set :session_secret, 'password_security'
     enable :sessions
   end
 end
