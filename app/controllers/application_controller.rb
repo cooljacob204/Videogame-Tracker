65bdeaeb9fa4c3d9ae5d4cb0632eec90ac43cc3a
Jacob Feldman <jacobgfeld@gmail.com>
2019-04-01 20:34:59 -0400

    Index grabs user IP for controller reference

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,31 @@ class ApplicationController < MyApp
       erb :index_loggedout
     end
   end
+
+  get '/connect' do
+    user = authenticate(session)
+    if user.approved?
+      #...
+    else
+      redirect '/'
+    end
+  end
+
+  post '/local_ip' do
+    user = authenticate(session)
+    user.update(ip_addr: params[:ip]) if user
+  end
+
+  def authorize_guest(mac)
+    url = ENV['CONTROLLER_URL']
+    username = ENV['CONTROLLER_PASSWORD']
+    password = ENV['CONTROLLER_USER']
+    site_id = ENV['CONTROLLER_SITE']
+
+    unifi = UnifiApi::Unifi.new(username: username, password: password, url: url)
+    unifi.login
+    unifi.site_find_by_id(site_id).authorize_guest(mac)
+
+    unifi.logout
+  end
 end
---
+++ db/migrate/20190402002953_add_ip_addr_to_users.rb
@@ -0,0 +1,5 @@
+class AddIpAddrToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :ip_addr, :text
+  end
+end
