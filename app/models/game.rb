abd79c3f4ae1b9a5b2283ecd948dc53b55de8621
Jacob Feldman <jacobgfeld@gmail.com>
2019-04-12 15:09:12 -0400

    Validates presence of game name

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ActiveRecord::Base
   has_many :user_games
   has_many :users, :through => :user_games
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
 end
