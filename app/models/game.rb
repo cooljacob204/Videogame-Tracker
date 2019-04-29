bdd660bdc051cf508a98b0a590e3f8bca662f76d
Jacob Feldman <jacobgfeld@gmail.com>
2019-04-29 18:01:08 -0400

    Game has a creator

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ActiveRecord::Base
   has_many :user_games
   has_many :users, :through => :user_games
+  belongs_to :creator, foreign_key: :user_id, class_name: :User
 
   validates :name, uniqueness: true, presence: true
   validates :user_id, presence: true
