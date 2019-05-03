b6f6d78070b71420a302d7b864bf69ad5317b8ca
Jacob Feldman <jacobgfeld@gmail.com>
2019-05-03 12:13:38 -0400

    Cleans up index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < MyApp
   get '/' do
-    if authenticate(session)
-      erb :index
-    else
-      erb :index_loggedout
-    end
+    authenticate(session)
+    erb :index
   end
 
   get '/games' do
