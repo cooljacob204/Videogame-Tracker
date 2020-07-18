ead39a70f8d15ef905b89f36ceee80073673a8ab
Jacob Feldman <me@jacobfeldman.dev>
2020-07-17 22:08:59 -0400

    Fixes spelling error

--- app/middlewares/media_validation.rb
+++ app/middlewares/media_validation.rb
@@ -11,11 +11,14 @@ class MediaValidation
     allowed_networks = IPAddr.new("10.0.0.0/8")
     
     return @app.call(env) unless req.path.match(/metrics/)
+
+    require 'pry'
+    binding.pry
     
     if allowed_networks===IPAddr.new(req.ip)
       @app.call(env)
     else
-      [ 401, { "Content-Type" => "text/plain" }, ["401: unauthroized\n"] ]
+      [ 401, { "Content-Type" => "text/plain" }, ["401: unauthorized\n"] ]
     end
   end
 end
